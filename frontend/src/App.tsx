e369b7f17ff0386b11f0bb6ccfe7ce45c75aca25
sandun24-is <sandunanjana08@gmail.com>
2026-03-27 15:39:59 +0530

    Add new routes for doctors and user pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Appointment from "./pages/Appointment";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import MyAppointments from "./pages/MyAppointments";
+import DoctorDetails from "./pages/DoctorDetails";
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:id" element={<DoctorDetails />} />
+
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/my-profile" element={<Profile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
       </Routes>
     </Layout>
   );
